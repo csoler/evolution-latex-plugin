6fdb0fdec1dcc220382468aa9e2b5b1e4caacdd6
Cyril Soler <csoler@users.sourceforge.net>
2025-12-18 13:35:39 +0100

    upload to new ppa

--- scripts/ppa_upload.sh
+++ scripts/ppa_upload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-Apply dput ppa:csoler-users/evolution-latex-plugin evolution-plugin-latex-equations_1.0.0-0.#_source.changes
+Apply dput ppa:csoler-users/evolution-plugins evolution-plugin-latex-equations_1.0.0-0.#_source.changes
 
